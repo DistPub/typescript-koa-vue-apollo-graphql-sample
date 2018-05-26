094d6788aa2636c3a2948d35c737cd717db5c817
Smite Chow <xiaopengyou2no1@gmail.com>
2018-05-26 11:38:37 +0800

    use separate file to support dev command

--- server_source/dev_index.ts
+++ server_source/dev_index.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import { ApolloEngine } from 'apollo-engine';
+import * as middleware from 'koa-webpack';
 
+app.use(middleware({}));
 const engine = new ApolloEngine({
     reporting: {
         disabled: true,
